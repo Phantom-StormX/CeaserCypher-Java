f9df198f0013e943e349432b99a2b6d4f8f066b1
impichler <impichler@thewildcats.net>
2026-04-24 14:22:22 -0400

    removed random spaces and added a few comments explaining the line(s) of code

--- src/main/java/com/phantomstormx/ceasercypherjava/CipherEngine.java
+++ src/main/java/com/phantomstormx/ceasercypherjava/CipherEngine.java
@@ -6,50 +6,41 @@ public class CipherEngine {
         // Handling only lowercase strings
         text = text.toLowerCase();
 
-        // Final cipher text
-
-        for (int j = 0; j < text.length(); j++) {
+        for (int i = 0; i < text.length(); i++) {
             // Determine the character position in alpha
-            int charPosition = alpha.indexOf(text.charAt(j));
-
+            int charPosition = alpha.indexOf(text.charAt(i));
             // Decryption
             int keyVal = (charPosition - shift) % 26;
             if (keyVal < 0) {
                 keyVal = alpha.length() + keyVal;
             }
-
             // Append the decrypted character
             char replaceVal = alpha.charAt(keyVal);
             text += replaceVal;
         }
+
+        // returns a deciphered text
         return text;
     }
-// returns a string as the result
 
-
+    // Used as a reference for Caesar ciphers to map characters to new positions.
     public static final String alpha = "abcdefghijklmnopqrstuvwxyz";
 
     public static StringBuilder encrypt(String text, int shift) {
         // Handling only lowercase strings
         text = text.toLowerCase();
 
-        // Final cipher text
         String cipherText = "";
-
         for (int i = 0; i < text.length(); i++) {
             // Determine the character position in alpha
             int charPosition = alpha.indexOf(text.charAt(i));
-
             // Encryption
             int keyVal = (shift + charPosition) % 26;
-
             // Encrypted character
             char replaceChar = alpha.charAt(keyVal);
-
             // Append the encrypted character
             cipherText += replaceChar;
         }
-
         // Return ciphered text
         return new StringBuilder(cipherText);
     }
